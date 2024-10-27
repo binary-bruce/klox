e0574ecaee57b76d2637f6d7d14896a7343266b6
binary.bruce <myth.litao@foxmail.com>
2024-10-27 12:12:15 +0800

    test

---
+++ interpreter/src/test/kotlin/klox/parser/ParserTest.kt
@@ -0,0 +1,43 @@
+package klox.parser
+
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertDoesNotThrow
+
+class ParserTest {
+
+    @Test
+    fun testParse() {
+        val script = """
+            for (var i = 1; i < 5; i = i + 1) {
+              print i * i;
+            }
+
+            class Duck {
+              fun init(name) {
+                this.name = name;
+              }
+
+              fun quack() {
+                print this.name + " quacks";
+              }
+            }
+
+            var duck = Duck("Waddles");
+            duck.quack();
+
+            fun make_adder(n) {
+              fun adder(i) {
+                return n + i;
+              }
+              return adder;
+            }
+            var add5 = make_adder(5);
+            print add5(1);
+            print add5(100);
+        """.trimIndent()
+
+        assertDoesNotThrow {
+            parse(script)
+        }
+    }
+}
