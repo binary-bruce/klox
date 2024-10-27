5b25817711997effa13110efe8e08b84c1ddaea4
binary.bruce <myth.litao@foxmail.com>
2024-10-27 17:42:51 +0800

    feat: the framework of the interpreter

--- interpreter/src/main/kotlin/klox/Lox.kt
+++ interpreter/src/main/kotlin/klox/Lox.kt
@@ -1,10 +1,36 @@
 package klox
 
+import klox.ast.Stmt
+import klox.parser.Parser
+import klox.resolver.Resolver
+import klox.scanner.Scanner
 import klox.scanner.Token
 import klox.scanner.TokenType
 
 object Lox {
     private var hadError = false
+    private var hadRuntimeError = false
+    private val interpreter = Interpreter()
+
+    fun run(source: String) {
+        val statements = source.toStatements()
+        val resolver = Resolver(interpreter)
+        resolver.resolve(statements)
+
+        if (hadError) return
+
+        interpreter.interpret(statements)
+    }
+
+    private fun String.toStatements(): List<Stmt> {
+        val tokens = Scanner(this).scanTokens()
+        return Parser(tokens).parse()
+    }
+
+    fun runtimeError(error: RuntimeError) {
+        System.err.println("${error.message}\n[line ${error.token.line}]")
+        hadRuntimeError = true
+    }
 
     fun error(line: Int, message: String) {
         report(line, "", message)
